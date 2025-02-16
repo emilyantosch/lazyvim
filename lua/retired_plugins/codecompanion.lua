f319cd9249a54cfcf9b03732ce80bd366852a6f8
emilyantosch <emilyluciaantosch@web.de>
2025-02-16 19:56:06 +0100

    Add(colorscheme): gruvbox and seethrough

--- lua/retired_plugins/codecompanion.lua
+++ lua/retired_plugins/codecompanion.lua
@@ -1,38 +1,38 @@
 return {
-{
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = true,
-  opts = {
-    strategies = {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = true,
+    opts = {
+      strategies = {
         chat = {
-          adapter = "qwencoder"
+          adapter = "qwencoder",
         },
         inline = {
-          adapter = "qwencoder"
+          adapter = "qwencoder",
         },
       },
-    adapters = {
+      adapters = {
         qwencoder = function()
           return require("codecompanion.adapters").extend("ollama", {
             name = "qwencoder",
             schema = {
               model = {
-                default = "qwen2.5-coder:14b"
+                default = "qwen2.5-coder:14b",
               },
               num_ctx = {
                 default = 16384,
               },
               num_predict = {
-                default = -1
-              }
-            }
+                default = -1,
+              },
+            },
           })
         end,
       },
     },
-},
+  },
 }
