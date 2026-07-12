005f35d60095b7e8b0e754632d6e6a4abd90965f
Emily Lucia Antosch <emilylucia.antosch@moia.io>
2026-07-12 03:27:49 +0200

    Add: pi plugin

--- lua/plugins/pi.lua
+++ lua/plugins/pi.lua
@@ -1,11 +1,6 @@
 return {
   {
-    "pablopunk/pi.nvim",
-    options = {
-      binary = "safepi",
-      provider = "openai-codex",
-      model = "openai-codex/gpt-5.5",
-      thinking = "low",
-    },
+    "carderne/pi-nvim",
+    opts = {},
   },
 }
